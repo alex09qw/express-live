0f38dfb2526aa42e7d8140064f585435f647431c
alex09qw <141842326+alex09qw@users.noreply.github.com>
2024-01-19 11:50:12 +0000

    new updates

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
+const mustacheExpress = require('mustache-express');
+const JOBS = require('./jobs');
+
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Configure mustache
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS});
 });
+
+app.get('/jobs/:id', (req, res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    res.render('job', { job: matchedJob});
+})
+
+app.post('/jobs/:id/apply', (req, res) => {
+    res.send("Got the application");
+})
 
 const port = process.env.PORT || 3000;
 
